5cc919fc90d8fea682f34af31ef8030a0902e5c0
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-24 16:38:40 +0200

    solve iteration 3 and 4

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,12 +1,12 @@
-import placeholderImage from "./../assets/placeholder.png";
-import studentsData from "./../assets/students.json";
+import placeholderImage from './../assets/placeholder.png';
+import studentsData from './../assets/students.json';
+import { Link, useParams } from 'react-router-dom';
 
 function StudentDetailsPage() {
-
+  const { studentId } = useParams();
   // Find the current student profile by id.
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const studentId = "1";
   const studentProfile = studentsData.find((student) => student._id === studentId);
 
   return (
@@ -26,7 +26,7 @@ function StudentDetailsPage() {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-24 mb-4 border-b pb-4">
               <p className="text-left mb-2 border-b pb-2">
-                <strong>LinkedIn:</strong>{" "}
+                <strong>LinkedIn:</strong>{' '}
                 <a
                   href={studentProfile.linkedinUrl}
                   target="_blank"
@@ -38,15 +38,12 @@ function StudentDetailsPage() {
               </p>
 
               <p className="text-left mb-2 border-b pb-2">
-                <strong>Email:</strong>{" "}
-                <span className="ml-2 text-blue-500 hover:underline">
-                  {studentProfile.email}
-                </span>
+                <strong>Email:</strong>{' '}
+                <span className="ml-2 text-blue-500 hover:underline">{studentProfile.email}</span>
               </p>
 
               <p className="text-left mb-2 border-b pb-2">
-                <strong>Languages:</strong>{" "}
-                {studentProfile.languages.join(", ")}
+                <strong>Languages:</strong> {studentProfile.languages.join(', ')}
               </p>
 
               <p className="text-left mb-2 border-b pb-2">
@@ -59,18 +56,14 @@ function StudentDetailsPage() {
 
               <p className="text-left mb-2 pb-2">
                 <strong>Cohort:</strong>
-                <span className="ml-2 text-blue-500 hover:underline">
-                  {studentProfile.cohort}
-                </span>
+                <span className="ml-2 text-blue-500 hover:underline">{studentProfile.cohort}</span>
               </p>
             </div>
 
-
             {/* Back button */}
             <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
-              Back
+              <Link to="/">Back</Link>
             </button>
-
           </>
         )}
       </div>
--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function UserProfilePage() {
   const userProfile = {
-    image: "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png",
-    name: "Jane Doe",
-    email: "janed@example.com",
-    role: "Lead Teacher",
+    image: 'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png',
+    name: 'Jane Doe',
+    email: 'janed@example.com',
+    role: 'Lead Teacher',
   };
 
   return (
@@ -17,9 +19,7 @@ function UserProfilePage() {
               alt="profile-photo"
               className="rounded-full w-32 h-32 object-cover border-2 border-gray-300"
             />
-            <h1 className="text-2xl mt-4 font-bold absolute">
-              {userProfile.name}
-            </h1>
+            <h1 className="text-2xl mt-4 font-bold absolute">{userProfile.name}</h1>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-24 mb-4 border-b pb-4">
               <p className="text-left mb-2 pb-2">
@@ -32,11 +32,10 @@ function UserProfilePage() {
           </>
         )}
 
-        {/* Back button */}        
+        {/* Back button */}
         <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
-          Back
+          <Link to="/">Back</Link>
         </button>
-        
       </div>
     </div>
   );
